721891585110ca41daf135a6db4271add87da778
Rafał Kuszka <rafal.kuszka@studenci.collegiumwitelona.pl>
2026-02-05 19:37:46 +0100

    Add Angular frontend, backend and device refactor
    project structure refactor
    Add angular
    Add backend

---
+++ gos-backend/src/main/java/com/rafal/gos_backend/GosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rafal.gos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GosBackendApplication.class, args);
+	}
+
+}
---
+++ gos-backend/src/test/java/com/rafal/gos_backend/GosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafal.gos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
